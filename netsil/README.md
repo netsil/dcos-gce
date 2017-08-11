8ebbaa25fd0ebce2005ab1ec7ad7d5e65be681d7
Kevin Lu <kevin@netsil.com>
2017-08-11 19:26:35 +0000

    robustness

---
+++ netsil/README.md
@@ -0,0 +1,2 @@
+* Export the variables `APPS_DIR` and `BUILDER_BRANCH`
+* Run `./netsil/reconfigure-dcos.sh`
---
+++ netsil/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export APPS_DIR=${APPS_DIR:-"/home/centos/netsil-dcos-gce/netsil/netsil-builder/apps"}
+
+# Install DCOS masters
+ansible-playbook -i hosts install.yml
+
+# Install DCOS agents
+ansible-playbook -i hosts add_agents.yml --extra-vars "start_id=0001 end_id=0002 agent_type=private"
+
+# Clone netsil-builder repo
+./netsil/clone-builder.sh
+
+# Reconfigure master and agent and start netsil cloud
+ansible-playbook -i hosts netsil/netsil-builder/ansible/cloud-deployment.yml
+
