b7bac9448bb629d71aef46eaa2be2b5bbd215680
Kevin Lu <kevin@netsil.com>
2017-08-25 00:16:32 +0000

    last two steps for lb

--- netsil/gcloud/06-create-forwarding-rule.sh
+++ netsil/gcloud/06-create-forwarding-rule.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -x
 # Create IP address
 gcloud compute addresses create netsil-cloud-address --global
 
-# TODO: Get address, put it in netsil_cloud_address 
+# Get address, put it in netsil_cloud_address 
+netsil_cloud_address=$(gcloud compute addresses describe netsil-cloud-address --global --format=json | jq --raw-output '.address')
 
 # Create forwarding rule
-gcloud compute forwarding-rules create netsil-cloud-https-forwarding-rule --global \ 
-	--address $netsil_cloud_address \
+gcloud compute forwarding-rules create netsil-cloud-https-forwarding-rule --global --address $netsil_cloud_address \
 	--ip-protocol TCP --port-range 443 \
 	--target-https-proxy netsil-cloud-https-proxy
 
