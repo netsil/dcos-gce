8513621e046cf0443a94281ebb3778ef3346ec88
Kevin Lu <kevin@netsil.com>
2017-08-25 23:57:11 +0000

    move some stuff around

---
+++ netsil/gcloud/lb/02-create-backend-services.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+gcloud compute instance-groups set-named-ports netsil-cloud-agent-ig \
+    --named-ports fe-port:80 \
+    --zone us-east1-b 
+
+gcloud compute instance-groups set-named-ports netsil-cloud-agent-ig \
+    --named-ports stats-port:2001 \
+    --zone us-east1-b
+
+gcloud compute backend-services create netsil-cloud-agent-backend-svc-fe \
+    --http-health-checks netsil-cloud-agent-healthcheck-fe --port-name fe-port --protocol HTTP \
+    --global
+    
+gcloud compute backend-services create netsil-cloud-agent-backend-svc-stats \
+    --http-health-checks netsil-cloud-agent-healthcheck-stats --port-name stats-port --protocol HTTP \
+    --global
---
+++ netsil/gcloud/sql/00-create-userdb.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# TODO: require ssl?
+# TODO: How do binary logging and HA/failover replica work?
+# TODO: How does restoration work?
+# TODO: Any cloud sql flags to add?
+
+# Get static IP addresses of agents
+
+# Create the CloudSQL instance
+gcloud sql instances create netsil-cloud-userdb \
+       --database-version=MYSQL_5_7 \
+       --region=us-east1 \
+       --gce-zone=us-east1-b \
+       --tier=db-n1-standard-8 \
+       --storage-auto-increase \
+       --storage-type=SSD \
+       --storage-size=500 \
+       --backup \
+       --backup-start-time=10:00 \
+       --enable-bin-log \
+       --failover-replica-name=netsil-cloud-userdb-failover-replica \
+       --authorized-networks=35.196.209.188,35.196.105.109 \
+       --maintenance-window-day=SAT \
+       --maintenance-window-hour=23
+
+# Store this password in 1pass and read it in when running the command below
+# Create root user
+gcloud sql users create root % \
+	--instance=netsil-cloud-userdb \
+	--password=cfEhfsNNl84Gh4oE
+
