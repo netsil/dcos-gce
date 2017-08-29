f91e87cf46caff12eb00f136ca5ebc93dda5c05e
Kevin Lu <kevin@netsil.com>
2017-08-29 18:33:58 +0000

    hash

--- netsil/gcloud/sql/02-create-netsil-db.sh
+++ netsil/gcloud/sql/02-create-netsil-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
-host=$(gcloud sql instances list --format=json | jq --raw-output '.[] | select(.name=="$CLOUDSQL_NAME") | .ipAddresses | .[].ipAddress')
+# TODO: Need a way to parameterize the name
+host=$(gcloud sql instances list --format=json | jq --raw-output '.[] | select(.name=="netsil-cloud-userdb-rev1") | .ipAddresses | .[].ipAddress')
 passwd=`cat userdb-passwd-file`
 mysql -h $host -u root -p$passwd -e 'CREATE DATABASE IF NOT EXISTS netsil'
